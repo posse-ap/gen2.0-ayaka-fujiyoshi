0e846dfd34bb86db75a4f80650a09051a0df9fca
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2021-07-24 18:42:24 +0900

    正解の解答ボックスの作成と表示の設定をした

--- kuizy/index.js
+++ kuizy/index.js
@@ -1,8 +1,21 @@
 'use strict';
 
-function displayAnswer(){
-    classList.add("answerBox");
-}
+//初期設定では答えを非表示
+document.getElementById("answerDisplay").style.display ="none";
 
-// ぷっしゅようの変更
-f
+function buttonClick(){
+    document.getElementById('answerChoice1-2')
+    // ボタンの変更
+    .classList.add('answerBox');
+
+    //答えの表示設定
+    const p1 = document.getElementById("answerDisplay");
+	if(p1.style.display=="block"){
+		// noneで非表示
+		p1.style.display ="none";
+	}else{
+		// blockで表示
+		p1.style.display ="block";
+	}
+
+}
