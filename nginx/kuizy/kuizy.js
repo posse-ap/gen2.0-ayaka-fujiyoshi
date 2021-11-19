f70f645678f1b8c6d3b710b09a3aa80650864712
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2021-11-19 22:36:15 +0900

    imgファイルの追加、それに伴うJSのimgの書き換え

--- nginx/kuizy/kuizy.js
+++ nginx/kuizy/kuizy.js
@@ -17,16 +17,16 @@ const optionGroups = [
 const answerGroups = ["たかなわ","かめいど","こうじまち","おなりもん","とどろき","しゃくじい","ぞうしき","おかちまち","ししぼね","こぐれ"]
 
 let questionPhotos = [
-['https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png'],
-['https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png'],
-['https://d1khcm40x1j0f.cloudfront.net/quiz/ad4f8badd896f1a9b527c530ebf8ac7f.png'],
-['https://d1khcm40x1j0f.cloudfront.net/quiz/ee645c9f43be1ab3992d121ee9e780fb.png'],
-['https://d1khcm40x1j0f.cloudfront.net/quiz/6a235aaa10f0bd3ca57871f76907797b.png'],
-['https://d1khcm40x1j0f.cloudfront.net/quiz/0b6789cf496fb75191edf1e3a6e05039.png'],
-['https://d1khcm40x1j0f.cloudfront.net/quiz/23e698eec548ff20a4f7969ca8823c53.png'],
-['https://d1khcm40x1j0f.cloudfront.net/quiz/50a753d151d35f8602d2c3e2790ea6e4.png'],
-['https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d812ea26e.png'],
-['https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png'],
+['img/takanawa.png'],
+['img/kameido.png'],
+['img/koujimachi.png'],
+['img/onarimon.png'],
+['img/todoroki.png'],
+['img/syakujii.png'],
+['img/zoshiki.png'],
+['img/okachimachi.png'],
+['img/shishibone.png'],
+['img/kogure.png'],
 ];
 
 //フィッシャー・イェーツのシャッフル
