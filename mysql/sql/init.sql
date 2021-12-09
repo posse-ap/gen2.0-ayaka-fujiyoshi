10b64741cefe1125230d560571d7b3cec3ec2c33
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2021-12-10 01:30:40 +0900

    テーブル初期データの書き方を変更、反映された

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,6 +1,14 @@
-DROP DATABASE IF EXISTS test_db;
+DROP DATABASE IF EXISTS quizy;
 CREATE DATABASE quizy;
+
 USE quizy;
+
 DROP TABLE IF EXISTS big_questions;
-CREATE TABLE big_questions ( id INT, name VARCHAR(50)) DEFAULT CHARACTER SET=utf8;
-INSERT INTO big_questions VALUES (1,'東京の難読地名クイズ');
+CREATE TABLE big_questions
+(
+  id           INT(50),
+  name     VARCHAR(50)
+);
+
+INSERT INTO big_questions (id, name) VALUES (1, "東京の難読地名クイズ");
+INSERT INTO big_questions (id, name) VALUES (2, "広島県の難読地名クイズ");
