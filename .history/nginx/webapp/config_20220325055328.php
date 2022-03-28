212fb3f84e7e35526bac7e6cbf0f3cf44f2eac02
{ayaka-fujiyoshi} <ayaka1712pome@gmail.com>
2022-03-29 01:04:34 +0900

    post処理をechoで確認できた

---
+++ .history/nginx/webapp/config_20220325055328.php
@@ -0,0 +1,12 @@
+<?php
+$DATABASE_NAME = "webapp";
+$CHARACTER_CODE = "charset=utf8mb4";
+$PDO_DSN = "mysql:host=mysql;dbname=" . $DATABASE_NAME . ";" . $CHARACTER_CODE . ";";
+         // ↑DB_HOSTはserviceの名前, 今回はdocker-compose.ymlでmysqlと命名している
+         // 連結演算子「.」で文字列と変数を結合
+$DB_USERNAME = "mysql";
+$DB_PASSWORD = "password";  
+$OPTIONS = array(
+   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+));
+// error_reporting(E_ALL & ~E_NOTICE);
