02451d869a163bb028401366d116122917e2442d
ayaka-fujiyoshi <ayaka1712pome@gmail.com>
2023-03-18 02:42:35 +0900

    追加機能実装可能

--- chap09/vuex-books/src/App.vue
+++ chap09/vuex-books/src/App.vue
@@ -1,102 +1,78 @@
 <template>
   <div>
-  <form v-on:submit.prevent="onclick">
-    <label for="isbn">ISBN：</label>
-    <input type="text" id="isbn" v-model="isbn" /><br />
-    <label for="title">書名：</label>
-    <input type="text" id="title" v-model="title" /><br />
-    <label for="price">価格：</label>
-    <input type="number" id="price" v-model="price" /><br />
-    <input type="submit" value="登録" />
-  </form>
-  <hr />
+    <form v-on:submit.prevent="onclick">
+      <label for="isbn">ISBN：</label>
+      <input type="text" id="isbn" v-model="isbn" /><br />
+      <label for="title">書名：</label>
+      <input type="text" id="title" v-model="title" /><br />
+      <label for="price">価格：</label>
+      <input type="number" id="price" v-model="price" /><br />
+      <input type="submit" v-if="isEditing" value="更新" />
+      <input type="submit" v-else value="登録" />
+
+    </form>
+    <hr />
     <p>書籍は全部で{{booksCount}}冊あります。</p>
-    <ul v-for="b of getBooksByPrice(2500)" v-bind:key="b.isbn">
-      <li>{{b.title}}（{{b.price}}円）<br />ISBN：{{b.isbn}} <br /><a href="">編集</a></li>
+    <ul v-for="book of getBooksByPrice(2500)" v-bind:key="book.isbn">
+      <li>
+        {{book.title}}（{{book.price}}円）<br />ISBN：{{book.isbn}} <br />
+        <a v-on:click="editBook(book)">[編集]</a>
+      </li>
     </ul>
-    <!-- <p>3000円未満の書籍は全部で{{booksCount3000}}冊あります。</p> -->
   </div>
 </template>
-
 <script>
-import { mapGetters } from 'vuex'
-// import { mapActions } from 'vuex'
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   name: 'App',
   computed: mapGetters(['booksCount', 'getBooksByPrice']),
-  
-  // computed: mapGetters(['booksCount', 'getBooksByPrice', 'booksCount3000']),
-
-  // computed: {
-  //   booksCount() {
-  //     return this.$store.getters.booksCount;
-  //   }
-  // },
-  // methods: {
-  //   getBooksByPrice(price) {
-  //     return this.$store.getters.getBooksByPrice(price)
-  //   }
-  // }
 
   data() {
     return {
       isbn: '',
       title: '',
-      price: 0
-    }
+      price: 0,
+      isEditing: false,
+      editedBook: null
+    };
   },
-  methods: {  
-    // ...mapActions([ 'addAsync' ]),
-    // ...mapActions({ add: 'addAsync' }),
+
+  methods: {
+    ...mapActions(['addAsync', 'updateAsync']),
 
     onclick() {
-      //mapActionsヘルパーを使う場合
-      // this.addAsync({
-      //   book: { isbn: this.isbn, title: this.title, price: this.price }
-      // })
-      // this.add({
-      //   book: { isbn: this.isbn, title: this.title, price: this.price }
-      // })
-      //this.addAsync({
-      // this.add({
-      //   book: {
-      //     isbn: this.isbn, title: this.title, price: this.price
-      //   }
-      // })
+      if (this.isEditing) {
+        this.updateAsync({
+          book: {
+            ...this.editedBook,
+            isbn: this.isbn,
+            title: this.title,
+            price: this.price
+          }
+        });
+         this.isEditing = false;
+         this.editedBook.isbn = this.isbn;
+         this.editedBook.title = this.title;
+         this.editedBook.price = this.price;
+         this.editedBook = null;
+      } else {
+        this.addAsync({
+          book: { isbn: this.isbn, title: this.title, price: this.price }
+        });
+      }
+      this.isbn = '';
+      this.title = '';
+      this.price = 0;
+    },
 
-      // this.$store.commit('addBook', {
-      //   book: {
-      //     isbn: this.isbn, title: this.title, price: this.price
-      //   }
-      // })
-
-      
-      //オブジェクト形式でのcommitメソッド呼び出し
-      // this.$store.commit({
-      //   type: 'addBook',
-      //   book: {
-      //     isbn: this.isbn, title: this.title, price: this.price
-      //   }
-      // })      
-
-      //addAsyncアクションの呼び出し
-      this.$store.dispatch('addAsync', {
-        book: {
-          isbn: this.isbn, title: this.title, price: this.price
-        }
-      })
-      
-      // オブジェクトリテラルにまとめてaddAsyncアクションの呼び出し
-      // this.$store.dispatch({
-      //   type: 'addAsync',
-      //   book: {
-      //     isbn: this.isbn, title: this.title, price: this.price
-      //   }
-      // })
-      
+    editBook(book) {
+      this.isbn = book.isbn;
+      this.title = book.title;
+      this.price = book.price;
+      this.isEditing = true;
+      this.editedBook = book;
     }
   }
-}
+};
 </script>
-
